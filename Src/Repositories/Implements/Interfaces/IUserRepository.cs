42cd9df24cd1d0ec91fe56f0389d1e2a0bb3214e
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-04-28 23:23:17 -0400

    feat: add IUserRepository in Repositories/Interfaces

---
+++ Src/Repositories/Implements/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+namespace Taller1IDWM.Src.Repositories.Interfaces;
+
+public interface IUserRepository
+{
+    Task<bool> UserExistsByEmailAsync(string email);
+    Task<bool> UserExistsByRutAsync(string rut);
+    Task<bool> SaveChangesAsync();
+}
