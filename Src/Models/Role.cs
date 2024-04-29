a5b2217ca5bebf39f4bc45eeaba1e58609419fb9
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-04-28 22:59:14 -0400

    feat: create Role and User models

---
+++ Src/Models/Role.cs
@@ -0,0 +1,7 @@
+namespace Taller1IDWM.Src.Models;
+
+public class Role
+{
+    public int Id {get; set;}
+    public required string Name {get; set;}
+}
---
+++ Src/Models/User.cs
@@ -0,0 +1,15 @@
+namespace Taller1IDWM.Src.Models;
+
+public class User
+{
+    public required string Rut {get;set;}
+    public required string Name {get;set;}
+    public required DateTime Birthdate {get;set;}
+    public required string Email {get;set;}
+    public required string Gender {get;set;}
+    public required byte[] PasswordHash {get; set;}
+    public required byte[] PasswordSalt {get; set;}
+    public int RoleId {get; set;}
+    public required Role Role {get;set;}
+
+}
