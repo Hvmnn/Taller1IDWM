6581a6757ee6fcf66e1839f9e5704a35301932d5
GitHubUser10F2C <nelson.soto@alumnos.ucn.cl>
2024-05-02 21:55:10 -0400

    fix: change type to category

--- Src/Models/Product.cs
+++ Src/Models/Product.cs
@@ -3,8 +3,9 @@ public class Product
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public required string Type { get; set; }
     public required int Price { get; set; }
     public required int Stock { get; set; }
     public required string ImageUrl { get; set; }
+    public int CategoryId {get; set;}
+    public required Category Category {get;set;}
 }
