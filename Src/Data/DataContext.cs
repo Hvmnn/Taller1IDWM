c9ced566e294ff4e19d87f181a61329b47a1da12
GitHubUser10F2C <nelson.soto@alumnos.ucn.cl>
2024-05-02 21:55:39 -0400

    feat: add category model

--- Src/Data/DataContext.cs
+++ Src/Data/DataContext.cs
@@ -8,7 +8,7 @@ namespace Taller1IDWM.Src.Data
         public DbSet<Product> Products { get; set;} = null!;
         public DbSet<User> Users {get; set;} = null!;
         public DbSet<Role> Roles {get; set;} = null!;
-        
+        public DbSet<Category> Categories {get; set;} = null!;
         public DataContext(DbContextOptions options) : base(options)
         {
             
