c808ac8f0d9d1c8de5cf911b1f41d6095935bdee
GitHubUser10F2C <nelson.soto@alumnos.ucn.cl>
2024-05-02 21:34:56 -0400

    feat: add category repository

---
+++ Src/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+using Taller1IDWM.Src.Models;
+
+namespace Taller1IDWM.Src.Repositories.Interfaces;
+public interface ICategoryRepository
+{
+    Task<Category?> GetCategoryById(int id);
+
+    Task<Category?> GetCategoryByName(string name);
+}
