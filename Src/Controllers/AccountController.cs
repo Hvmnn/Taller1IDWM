edca8bd1be3e8897ccbfa944348ca467c75debfe
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-05-11 18:06:54 -0400

    feat: add "Login" post method in AccountController.cs

--- Src/Controllers/AccountController.cs
+++ Src/Controllers/AccountController.cs
@@ -30,4 +30,25 @@ public class AccountController (IAccountRepository accountRepository, IUserRepos
 
         return TypedResults.Ok(accountDto);
     }
+
+    [HttpPost("login")]
+    public async Task<IResult> Login(LoginDto loginDto)
+    {
+        CredentialDto? credentialDto = await _accountRepository.GetCredentialAsync(loginDto.Email);
+
+        if(credentialDto is null)
+        {
+            return TypedResults.BadRequest("Credentials are invalid");            
+        }
+
+        var result = BCrypt.Net.BCrypt.Verify(loginDto.Password, credentialDto.Password);
+        if(!result)
+        {
+            return TypedResults.BadRequest("Credentials are invalid");
+        }
+
+        AccountDto? accountDto = await _accountRepository.GetAccountAsync(loginDto.Email);
+
+        return TypedResults.Ok(accountDto);
+    }
 }
