a3b819f0e99cdd9cef958dc7433577327abe31bb
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-05-11 18:06:00 -0400

    feat: add AccountController and "Register" post method

---
+++ Src/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Taller1IDWM.Src.DTOs.Account;
+using Taller1IDWM.Src.Repositories.Interfaces;
+
+namespace Taller1IDWM.Src.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController (IAccountRepository accountRepository, IUserRepository userRepository) : ControllerBase
+{
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly IAccountRepository _accountRepository = accountRepository;
+
+    [HttpPost("register")]
+    public async Task<IResult> Register(RegisterDto registerDto)
+    {
+        if(await _userRepository.UserExistsByEmailAsync(registerDto.Email) || await _userRepository.UserExistsByRutAsync(registerDto.Rut))
+        {
+            return TypedResults.BadRequest("User already exists");
+        }
+
+        await _accountRepository.AddAccountAsync(registerDto);
+
+        if(!await _accountRepository.SaveChangesAsync())
+        {
+            return TypedResults.BadRequest("Failed to save user");
+        }
+
+        AccountDto? accountDto = await _accountRepository.GetAccountAsync(registerDto.Email);
+
+        return TypedResults.Ok(accountDto);
+    }
+}
