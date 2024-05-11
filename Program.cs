9bb969736fc6a0cdf91a2ffd3fc784f322d6b366
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-05-11 18:01:38 -0400

    fix: add AutoMapper services and AccountRepository services in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 using Taller1IDWM.Src.Extensions;
+using Taller1IDWM.Src.Repositories.Implements;
+using Taller1IDWM.Src.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
