e36bce61804cc96376654c09ad7250593661f133
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-05-18 18:31:27 -0400

    feat: create EditUserDto

---
+++ Src/DTOs/User/EditUserDto.cs
@@ -0,0 +1,8 @@
+namespace Taller1IDWM.Src.DTOs.User;
+
+public class EditUserDto
+{
+    public required string Name{get; set;}
+    public required DateTime Birthdate {get;set;}
+    public required string Gender {get;set;} 
+}
