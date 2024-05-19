75e6a07f296700bed22c2a6274c09c942c7055d9
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-05-19 00:05:39 -0400

    feat: add UserController

--- Program.cs
+++ Program.cs
@@ -1,14 +1,37 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using Taller1IDWM.Src.Extensions;
 using Taller1IDWM.Src.Repositories.Implements;
 using Taller1IDWM.Src.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
+builder.Services.AddAuthentication().AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+            builder.Configuration.GetSection("AppSettings:Token").Value!))
+    };
+});
 
 var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.ConfigureApp();
 app.Run();
--- Src/Controllers/UserController.cs
+++ Src/Controllers/UserController.cs
@@ -1,13 +1,28 @@
+using System.Collections;
+using System.Security.Claims;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Taller1IDWM.Src.DTOs.User;
+using Taller1IDWM.Src.Models;
+using Taller1IDWM.Src.Repositories.Implements;
 using Taller1IDWM.Src.Repositories.Interfaces;
 
 namespace Taller1IDWM.Src.Controllers;
 
 [ApiController]
-[Authorize(Roles = "Admin")]
+//[Authorize(Roles = "Admin")]
 [Route("api/[controller]")]
-public class UserController : ControllerBase
+public class UserController (IUserRepository userRepository, IMapper mapper): ControllerBase
 {
-    private readonly IUserRepository _userRepository;
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly IMapper _mapper = mapper;
+
+    [HttpGet("users")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+    {
+        var users = await _userRepository.GetUsers();
+        var userDtos = _mapper.Map<IEnumerable<UserDto>>(users);
+        return Ok(userDtos);
+    }
 }
