3eb282960aa4c384a943c3705153ab7ef8731291
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2024-05-19 00:06:24 -0400

    fix: change required variables in EditUserDto

--- Src/DTOs/User/EditUserDto.cs
+++ Src/DTOs/User/EditUserDto.cs
@@ -2,7 +2,7 @@ namespace Taller1IDWM.Src.DTOs.User;
 
 public class EditUserDto
 {
-    public required string Name{get; set;}
-    public required DateTime Birthdate {get;set;}
-    public required string Gender {get;set;} 
+    public string Name{get; set;} = string.Empty;
+    public DateTime Birthdate {get;set;}
+    public string Gender {get;set;} = string.Empty;
 }
